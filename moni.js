f7fd2292667f0139bf01d51ad5ac3dbc17da95dd
MONIKANAMALA <namalamonika342@gmail.com>
2023-08-09 10:53:23 +0530

    added moni.js

---
+++ moni.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class moni extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hemanth</h1>
+      </div>
+    )
+  }
+}
